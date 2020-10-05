c17132976ac553803bec8631efb0601815f16fc6
lishaorong <mbigbug@163.com>
2020-10-05 18:30:10 +0800

    update:

--- controllers/assess.js
+++ controllers/assess.js
@@ -1,10 +1,15 @@
 let _ = require('lodash');
-let Assess = require("../models/AssessTask");
+let Assess = require('../models/AssessTask');
+let Organization = require('../models/Organization');
 
 exports.fetchUserAssessList = async function (req, res) {
     let user = req.user;
     try {
         let assesseeList = await Assess.find({assessee_id: user.organizationId});
+        let orgIds = _.chain(assesseeList).map(e=>e.assessor_id).uniq().value();
+        orgIds.push(req.user.organizationId)
+        const orgs = await Organization.find({_id: {$in: orgIds}});
+        const orgInfoMap = _.keyBy(orgs, '_id');
         assesseeList = _.chain(assesseeList).filter(e => !e.assessee_done).map(e => {
             return {
                 startTime: e.start_time,
@@ -12,10 +17,12 @@ exports.fetchUserAssessList = async function (req, res) {
                 name: e.name,
                 target: e.target,
                 content: e.template_content,
+                assesseeOrgName : orgInfoMap[e.assessee_id].name,
+                assessorOrgName : orgInfoMap[e.assessor_id].name,
                 type: '2',
                 createTime: e.createdAt
             }
-        });
+        }).value();
         let assessorList = await Assess.find({assesser_id: user.organizationId});
         
         // let data = _.chain(messageList).filter(e => !e.is_read).map(e => {
