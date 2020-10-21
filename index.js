38d832544e825e38a140ee90d5cb57615aca7a3f
lishaorong <mbigbug@163.com>
2020-10-21 23:07:53 +0800

    update:

--- index.js
+++ index.js
@@ -22,6 +22,15 @@ http.createServer(app).listen(HTTP_PORT, function () {
 mongoose.connect(config.database, { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(cors());
+
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By",' 3.2.1');
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
