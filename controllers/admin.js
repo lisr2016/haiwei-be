6b6cca020b245e710ad6ff4dde3eb54be6cf56a1
lishaorong <mbigbug@163.com>
2020-10-22 15:51:49 +0800

    update:

--- controllers/admin.js
+++ controllers/admin.js
@@ -969,8 +969,8 @@ exports.fetchAssessTaskList = async function (req, res) {
         let list = _.map(data, e => {
             return {
                 id: e._id,
-                startTime: e.start_time && e.start_time.getTime(),
-                endTime: e.end_time && e.end_time.getTime(),
+                startTime: formatTime(e.start_time && e.start_time.getTime()),
+                endTime: formatTime(e.end_time && e.end_time.getTime()),
                 name: e.name,
                 target: e.target,
                 content: e.template_content,
