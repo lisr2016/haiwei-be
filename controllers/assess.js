deec50cb44aea6b4cca251545ab165c257298370
lishaorong <mbigbug@163.com>
2020-10-24 22:48:59 +0800

    update:

--- controllers/assess.js
+++ controllers/assess.js
@@ -87,7 +87,7 @@ exports.uploadPics = async function (req, res) {
             return;
         }
         if (!(_.size(req.body.taskId) && _.size(req.body.contentIndex) && _.size(req.body.picIndex))) {
-            res.status(400).json({code: 5, msg: 'taskId、contentIndex 或 picIndex参数错误'});
+            res.status(400).json({code: 5, msg: 'taskId,contentIndex或picIndex参数错误'});
             return;
         }
         if (!req.body.filename) {
