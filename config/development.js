a601b9056d03cf22d9014d633ca1480a12f38d29
lishaorong <mbigbug@163.com>
2020-11-09 05:10:14 +0800

    update:

--- config/development.js
+++ config/development.js
@@ -2,5 +2,5 @@ module.exports = {
     port: 4000,
     secret:'r0V4uaqQRdZ7PNgR',
     cms_secret:'g3meYhPwcYCY5NIs',
-    database: 'mongodb://test:pnI3EkVpMVU8thy1@123.206.56.111:27017/hai_wei_pro'
+    database: 'mongodb://hai_wei_dev:WAtMRvB60uGEf81I@123.206.56.111:27017/hai_wei_dev'
 }
